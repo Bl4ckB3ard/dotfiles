ca9407a8fd28865eb9cda498f37327f18fe8cc73
Colton Thomas <coltonthomas523@gmail.com>
2022-12-09 15:09:18 -0700

    intial commit

---
+++ nvim/lua/Bl4ckb3ard/packer_config.lua
@@ -0,0 +1,64 @@
+return require("packer").startup(function()
+
+		---------------------
+        -- Package Manager --
+        ---------------------
+	use "wbthomason/packer.nvim"
+	
+		----------------------
+        --   Dependencies   --
+        ----------------------
+	use "nvim-lua/plenary.nvim"
+	-- devcons
+	use "kyazdani42/nvim-web-devicons"
+
+
+        ----------------------------------------------
+        -- Themes, Status Line, Buffer Tabs, Syntax --
+        ----------------------------------------------
+
+	-- gruvbox
+	use {"luisiacc/gruvbox-baby", branch = "main"}
+
+	-- lualine
+	use {"nvim-lualine/lualine.nvim", config = function()
+			require("Bl4ckb3ard.plugins.lualine")
+		end}
+
+	-- bufferline
+	use {"akinsho/bufferline.nvim", requre,tag = "v3.*", requires = "kyazdani42/nvim-web-devicons", config = function()
+			require("bufferline").setup()
+		end}
+	--syntax highliting
+	use "sheerun/vim-polyglot"
+
+		---------------------------
+        -- Functionality Plugins --
+		---------------------------
+
+	-- easy way to comment stuff out
+	use "tpope/vim-commentary"
+	
+	-- telescope
+	use {"nvim-telescope/telescope.nvim", tag = "0.1.0", config = function()
+			require("Bl4ckb3ard.plugins.telescope")
+		end}
+	
+		---------------------
+        --       LSP       --
+		---------------------
+
+	use "neovim/nvim-lspconfig"
+	
+	use "hrsh7th/nvim-cmp"
+	use "hrsh7th/cmp-buffer"
+	use "hrsh7th/cmp-path"
+	use "hrsh7th/cmp-nvim-lsp"
+
+	-- Autopairs
+	-- have to load autopairs after nvim-cmp
+	use {"windwp/nvim-autopairs", after = "nvim-cmp", config = function()
+			require("Bl4ckb3ard.plugins.pairs")
+		end}
+
+end)
